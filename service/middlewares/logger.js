3422743b7031606469903dcb9acef52e84b66841
Lakshman Siva <siva.l@northeastern.edu>
2025-01-28 20:39:57 -0500

    fix: update logger middleware
    Fixed logger middleware to check for and create the 'logs' directory if it doesn't exist.

--- service/middlewares/logger.js
+++ service/middlewares/logger.js
@@ -2,6 +2,12 @@ import morgan from "morgan";
 import fs from "fs";
 import path from "path";
 
+// Create the logs folder if it doesn't exist
+const logsDirectory = path.join("logs");
+if (!fs.existsSync(logsDirectory)) {
+    fs.mkdirSync(logsDirectory, { recursive: true });
+}
+
 // Create a writable stream for file logging
 const logStream = fs.createWriteStream(path.join("logs", "access.log"), {
     flags: "a",
