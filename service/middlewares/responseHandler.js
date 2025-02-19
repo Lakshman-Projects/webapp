dfb5a804ce0a47646b239c751be6074a27586374
Lakshman Siva <siva.l@northeastern.edu>
2025-02-19 13:06:06 -0500

    Revert API failure cases

--- service/middlewares/responseHandler.js
+++ service/middlewares/responseHandler.js
@@ -1,6 +1,19 @@
 
 const apiMiddleware = (req, res, next) => {
+    // Only GET is allowed
+    if (req.method !== "GET") {
+        return res.status(405).send();
+    }
 
+    // No payload is allowed
+    if (parseInt(req.headers["content-length"] || "0", 10) > 0) {
+        return res.status(400).send();
+    }
+
+    // No query parameters are allowed
+    if (Object.keys(req.query).length > 0) {
+        return res.status(400).send();
+    }
 
     next();
 };
