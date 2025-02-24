6ae212441793ef34548a308e62f8023fa34e7318
Lakshman Siva <slakshman664@gmail.com>
2025-02-24 18:41:21 -0500

    test commit 2

--- service/middlewares/responseHandler.js
+++ service/middlewares/responseHandler.js
@@ -5,7 +5,15 @@ const apiMiddleware = (req, res, next) => {
         return res.status(405).send();
     }
 
+    // No payload is allowed
+    if (parseInt(req.headers["content-length"] || "0", 10) > 0) {
+        return res.status(400).send();
+    }
 
+    // No query parameters are allowed
+    if (Object.keys(req.query).length > 0) {
+        return res.status(400).send();
+    }
 
     next();
 };
