de5491e55bbfe0d279098d0ad32abf8e0b238868
Lakshman Siva <siva.l@northeastern.edu>
2025-03-25 19:33:10 -0400

    Add metrics system for CloudWatch using statd
    Integrated statd to collect and send application metrics to AWS CloudWatch for monitoring and performance tracking.

--- service/services/healthz-service.js
+++ service/services/healthz-service.js
@@ -1,11 +1,12 @@
 import db from "../models/index.js";
+import { measureDbQuery } from "../middlewares/statd-metrics.js";
 
 const { HealthCheck } = await db;
 
 export const createHealthCheck = async () => {
     try {
         const datetime = new Date(); // Current UTC date and time
-        const healthCheckEntry = await HealthCheck.create({ datetime: datetime });
+        const healthCheckEntry = await measureDbQuery(() => HealthCheck.create({ datetime: datetime }), 'create_health_check');
         return healthCheckEntry;
     } catch (error) {
         throw error;
